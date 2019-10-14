8d1574fc0198ae24b97c89c3e1ae4e6204c309a1
Kwaku Prempeh <kwaku.prempeh@holition.com>
2019-10-14 22:52:09 +0100

    feat: page one added

--- apps/mvp-app/src/app/app.module.ts
+++ apps/mvp-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { PageOneComponent } from './page-one/page-one.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PageOneComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
