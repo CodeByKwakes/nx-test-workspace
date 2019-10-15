ab2da978f2afa62eca728ceb9b3fd5005dc23b5e
Kwaku Prempeh <kwaku.prempeh@holition.com>
2019-10-15 02:07:12 +0100

    chore: home module added
    BREAKING CHANGE: new module added

--- apps/mvp-app/src/app/app.module.ts
+++ apps/mvp-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { PageOneComponent } from './page-one/page-one.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent, PageOneComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
